681f9e7ce1998f06c0e631aec2e01e777a377e82
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-06 09:36:59 +0000

    Conversion operation

---
+++ conversionOperation.js
@@ -0,0 +1,53 @@
+
+let score="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+let n="33abc"
+console.log(typeof n)
+let value=Number(n)
+console.log(typeof value)
+console.log(value)  //NaN Not a Number
+
+let na=null
+console.log(typeof na)
+let valuee=Number(na)
+console.log(typeof valuee)
+console.log(valuee) //null value is zero
+
+let naa=undefined
+console.log(typeof naa)
+let valueee=Number(naa)
+console.log(typeof valueee)
+console.log(valueee)  //undefined print the NaN
+
+ //"33"=>33
+ //"33abc"=>NaN
+ //true=>1; false=>0;
+ 
+ let isLoggedIn=""
+
+ let booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn) //print false
+
+ //"""=>false;
+ //"bhuppi"=>true
+
+let v=3;
+let negv=-v;
+console.log(negv);
+
+
+console.log("1"+2);  //12
+console.log(1+"2");  //12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+console.log(+true) //1
+console.log(+"")  //0
+
+                              
