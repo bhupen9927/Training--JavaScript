f79334c672d3c870f717a8a38009a5c40f86d88c
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-08 05:31:57 +0000

    User Input

---
+++ userInput.js
@@ -0,0 +1,22 @@
+let name = readLine("What is your name?");
+let num = readInt("What is your favorite number? ");
+
+println("Hello " + name + "!");
+println(num + "?! That's my favorite number too!");
+
+// Prints out the variable type
+println("Name is a " + typeof name);
+println("Num is a " + typeof num);
+
+
+// Gets user input
+var Name = prompt("What is your name?");
+var Num = prompt("What is your favorite number? ");
+
+// Uses user input to print out information
+println("Hello " + Name + "!");
+println(Num + "?! That's my favorite number too!");
+
+// Prints out the variable type
+println("Name is a " + typeof Name);
+println("Num is a " + typeof Num);
