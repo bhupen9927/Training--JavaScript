6ff3fbe7fe6a7db2dc3f2a697e26a2aae28eabf6
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-08 05:03:37 +0000

    conditonal expression

--- conditional.js
+++ conditional.js
@@ -15,3 +15,49 @@ else{
 console.log("Done")
 // HomeWork - Explore switch statement and write a basic program in the comments
 console.log("You can", (a<18? "not drive" :"drive"))
+
+
+
+let age = prompt("What is your age?")
+age = Number.parseInt(age)
+if (age > 10 && age < 20) {
+  console.log("Your age lies between 10 and 20")
+}
+else {
+  console.log("Your age doesnt lies between 10 and 20")
+}
+
+
+let agee = prompt("What is your age?")
+switch (agee) {
+  case '12':
+    console.log("Your age is 12")
+    break
+  case '13':
+    console.log("Your age is 13")
+    break
+  case '14':
+    console.log("Your age is 14")
+    break
+  case '15':
+    console.log("Your age is 15")
+    break
+  default:
+    console.log("Your age is not special")
+}
+
+
+let num = prompt("What is your age?")
+num = Number.parseInt(num)
+if (num % 2 == 0 && num % 3 == 0) {
+  console.log("Your number is divisible by 2 and 3")
+}
+else {
+  console.log("Your number is not divisible by 2 and 3")
+}
+
+
+// Problem No 5
+let aged = 19
+let aa = age > 18 ? "You can drive" : "You cannot drive"
+console.log(aa)
