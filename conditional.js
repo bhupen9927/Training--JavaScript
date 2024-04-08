0aaaad879c2236c5aac89fc2677c0ccdc0cd9780
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-08 05:20:41 +0000

    conditional

--- conditional.js
+++ conditional.js
@@ -1,25 +1,27 @@
-let a = prompt("Hey whats you age?");
+let a = 14;  // prompt is not work here, it is work on browser console
 a = Number.parseInt(a); // Converting the string to a number
 if(a<0){
-  alert("This is an invalid age");
+  console.log("This is an invalid age");
 }
 else if(a<9){
-  alert("You are a kid and you cannot even think of driving");
+  console.log("You are a kid and you cannot even think of driving");
 }
 else if(a<18 && a>=9){
-  alert("You are a kid and you can think of driving after 18");
+  console.log("You are a kid and you can think of driving after 18");
 }
 else{
-  alert("You can now drive as you are above 18");
+  console.log("You can now drive as you are above 18");
 }
 console.log("Done")
-// HomeWork - Explore switch statement and write a basic program in the comments
+
 console.log("You can", (a<18? "not drive" :"drive"))
 
 
 
-let age = prompt("What is your age?")
+let age = 4
+console.log(typeof (age))
 age = Number.parseInt(age)
+console.log(typeof (age))
 if (age > 10 && age < 20) {
   console.log("Your age lies between 10 and 20")
 }
@@ -28,7 +30,7 @@ else {
 }
 
 
-let agee = prompt("What is your age?")
+let agee = 12
 switch (agee) {
   case '12':
     console.log("Your age is 12")
@@ -47,7 +49,7 @@ switch (agee) {
 }
 
 
-let num = prompt("What is your age?")
+let num = 57
 num = Number.parseInt(num)
 if (num % 2 == 0 && num % 3 == 0) {
   console.log("Your number is divisible by 2 and 3")
