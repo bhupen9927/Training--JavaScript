877765072f093645c2e581719b5ad99d85eaa0cd
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-10 11:37:05 +0000

    JS History points

---
+++ history.js
@@ -0,0 +1,92 @@
+// JavaScript is a versatile programming language commonly used in web development to enhance the user experiences on the internet. 
+// It is primarily utilised for implementing functionalities within our web applications. 
+// JavaScript is often hailed as a the "language of the web", is the most important part of modern web development.
+
+
+// ECMAScript is a standard that ensures consistency and uniformity in JavaScript behavior across different platforms or browsers.
+// It provides a set of rules and specifications for developing new features in JavaScript to maintain consistency in coding.
+
+// JavaScript follows standards set by Ecma Script
+
+// The console window allows you to execute JavaScript code directly in the browser.
+
+// Tracing the Evolution: A Concise History of JavaScript
+
+// initially, JavaScript was developed in just 10 days.
+// JavaScript was created by Brendan Eich, a programmer at a company called Netscape Communications. 
+
+// Originally, JavaScript was named 'Mocha', but it quickly gained popularity as 'LiveScript' and later evolved into a JvaScript'.
+
+// Initially, JavaScript was designed solely to provide scripting capabilities aimed at enhancing web pages with dynamic content and interactivity.
+// With the growth of HTML5 and CSS3, JavaScript's capabilities expanded further, enabling developers to construct sophisticated web applications
+// and interactive user interfaces.
+
+// Many JavaScript frameworks and libraries, such as AngularJS, ReactJS, and Vue.js, have revolutionized frontend development.
+// Additionally, Node.js, a powerful JavaScript runtime environment, has empowered developers to build server-side applications using JavaScript.
+
+
+// JavaScript : A Programming Language or a Scripting Language?
+
+// While studying JavaScript a common question arises: Is JavaScript a scripting language or it is a programming language?.
+
+// The tech community often debates whether JavaScript should be classified as scripting language or programming language. 
+// To grasp this debate lets understand the distinction between scripting languages and a programming languages.
+
+// Scripting Languages-
+
+// Scripting languages are those languages which are interpreted rather than compiled, meaning they are executed line-by-line by the interpreter
+// at the runtime thus allowing for quicker development cycles and easier debugging.
+// Scripting languages are commonly used for tasks such as system administration, web development, text processing and automation.
+
+// JavaScript is widely used for client-side scripting in web browsers, it has became a versatile language for web development including front-end
+// and server side scripting with platforms like Node.js. 
+
+// Scripting languages are interpreted within a host environment; for instance, JavaScript needs to be integrated into HTML and then interpreted by
+// the web browser.
+
+
+// Programming Languages-
+
+// Programming languages are compiled languages, which means the source code is compiled to convert it to machine code.
+// A programming language is a computer language which is used to communicate with computers using a set of instructions. 
+// Programming languages utilize compilers and do not rely on interpretation by another language or application.
+
+// conclusion-
+
+// All the scripting languages are programming languages. Technically, JavaScript falls under both categories. It shares traits scripting languages
+// but also possesses the capabilities of many general-purpose programming languages. At the end we can say that JavaScript is both a scripting
+// language and a light-weight programming language, that can be used for a wide range of tasks, from simple automation to complex application
+// development.
+
+
+// JavaScript : Is it a Client-Side or Server-Side Language?
+
+// For years, JavaScript has been predominantly associated with client-side scripting, breathing life into web pages and making them interactive.
+// JavaScript's roots lie firmly in the client-side realm. It's the language that adds the sparkle to your web experience, making elements appear
+// and disappear, reacting to your clicks and scrolls, and seamlessly handling forms and animations.
+
+// But in recent times, its dominion has expanded, blurring the lines between front-end and back-end.
+
+// In recent years, JavaScript has also gained prominence as a server-side language, thanks to the introduction of Node.js. Node.js is an 
+// open-source, JavaScript runtime environment that allows developers to run JavaScript code server-side. With Node.js, 
+// JavaScript can be used to build scalable, high-performance web applications, APIs, and backend services.
+
+// Hence, as the technology evolved and the new trends emerged, JavaScript excelled in both client-side interactivity and server-side logic. 
+// To call it simply "client-side" or "server-side" would be like calling water simply "wet" 
+// - it misses the clarity and versatility that truly define it.
+
+// JavaScript Engines for Executing JavaScript Code
+// A JavaScript engine is the software that takes our JavaScript code and translates it into instructions that your computer can understand. It is
+//  like a powerful interpreter.
+
+// Some of the most popular JS engines today are:
+
+// 1: V8 - Developed by Google, V8 is one of the most widely used JavaScript engines. It powers the JavaScript execution in Google Chrome and Node.js.
+
+// 2: SpiderMonkey - Developed by Mozilla Firefox, SpiderMonkey is the JavaScript engine used in the Firefox web browser. 
+// It was one of the first JavaScript engines ever created and is known for its performance and compliance with JavaScript standards.
+
+// 3: JavaScriptCore - Used in Safari, known for its tight integration with Apple devices.
+
+// 4: Chakra - Developed by Microsoft, Chakra was the JavaScript engine used in the Edge web browser. It has now been replaced by the Chromium-based
+// Edge browser, which uses V8 as its JavaScript engine.
