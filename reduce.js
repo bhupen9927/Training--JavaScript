a481fce31a238a123dd4ed4d54d52ee861cbda07
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-10 14:18:15 +0000

    reduce function

---
+++ reduce.js
@@ -0,0 +1,36 @@
+const nums=[1,2,3]
+
+const total=nums.reduce(function (acc,curr){
+    console.log(`acc:${acc} and current:${curr}`)
+    return acc+curr
+},0)
+
+console.log("total is ",total)
+
+//using arrow function
+
+const mytotal=nums.reduce((acc,curr) => acc+curr,0)
+console.log(mytotal)
+
+
+const array = [5, 1, 3, 2, 6];
+// Calculate sum of elements of array - Non functional programming way
+function findSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+console.log(findSum(array)); // 17
+
+// reduce function way
+const sumOfElem = arr.reduce(function (accumulator, current) {
+  // current represent the value of array
+  // accumulator is used the result from element of array.
+  // In comparison to previous code snippet, *sum* variable is *accumulator* and *arr[i]* is *current*
+  accumulator = accumulator + current;
+  return accumulator;
+}, 0); 
+//In above example sum was initialized with 0, so over here accumulator also needs to be initialized, so the second argument to reduce function represent the initialization value.
+console.log(sumOfElem); // 17
