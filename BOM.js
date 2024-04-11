3a005c2f9790416a2de62ab2b7867629cbb99067
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-11 14:24:34 +0000

    Browser Object Mode

---
+++ BOM.js
@@ -0,0 +1,31 @@
+// The console object gives you access to the browser console
+
+// This console allows the developers to log certain results and alerts and warnings to the screen
+
+// The Browser Object Model (BOM) allows JavaScript to "talk to" the browser.
+
+// What is window Object?
+// Window Object is supplied by what is known as the browser Object Model or you can say for short as BOM
+
+// the window object represents the browser window or, more broadly, the global execution context in which JavaScript code runs. 
+// It is a top-level object in the browser's JavaScript environment and serves as the global object for JavaScript code running in a web browser.
+
+// The window object provides access to various properties and methods related to the browser window, such as the document object 
+// (window.document), which represents the DOM (Document Object Model) of the currently loaded web page. 
+// Additionally, window contains other properties like location, history, navigator, and methods like alert(), setTimeout(), and setInterval().
+
+// Since window is the global object in the browser environment, you can access its properties and methods directly without specifying window 
+// explicitly. For example, window.alert("Hello") and alert("Hello") are equivalent.
+
+// // Accessing window properties
+// console.log(window.innerWidth); // Current inner width of the browser window
+// console.log(window.location.href); // Current URL of the page
+
+// // Accessing window methods
+// window.alert("This is an alert!"); // Displays an alert dialog
+
+
+// window object is the root element of the whole Website 
+//  window provides two broad categories of functionality to us 
+
+//  It provides us with the document. Document means it's the website like titles on the screen, buttons we can click on etc.
