c2c1d4f3eb412de2f1357c90e9da84497a6c48bc
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-11 14:24:16 +0000

    DOM

---
+++ DOM.js
@@ -0,0 +1,12 @@
+// Just understand that almost every single interaction on your page, for example like slideshow on a carousel, navigating menu items
+// all are result of the javascript accessing and manipulating the DOM
+
+// Where does the DOM Come from?
+
+// The Dom doesnt comes from Javascript. The browser gives us the access to the DOM. This means that different browsers will have their own
+// unique way of creating the DOM.
+
+// Everything in the browser can be represented as an Object
+
+// When we start accessing this document Object we have a whole lot of properties and methods available to us straight out of the box, and we
+// could access these properties and methods
