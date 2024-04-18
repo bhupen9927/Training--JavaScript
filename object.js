e1565eac0c694d68c5db8300893a438164c183cd
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-04-18 14:15:32 +0000

    object

--- object.js
+++ object.js
@@ -52,3 +52,72 @@ const myObject = {
 console.log(myObject[mySymbol]); // Outputs: Symbol property value
 console.log(typeof(myObject[mySymbol]))
 console.log(typeof(mySymbol))
+
+
+let car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2020,
+  color: "blue"
+};
+console.log(car.brand);  // Outputs: Toyota
+console.log(car['model']); // Outputs: Corolla
+
+
+let calculator = {
+  operand1: 0,
+  operand2: 0,
+  add: function() {
+    return this.operand1 + this.operand2;
+  },
+  subtract: function() {
+    return this.operand1 - this.operand2;
+  },
+  setOperands: function(op1, op2) {
+    this.operand1 = op1;
+    this.operand2 = op2;
+  }
+};
+
+calculator.setOperands(5, 3);
+console.log(calculator.add());       // Outputs: 8 (5 + 3)
+console.log(calculator.subtract());  // Outputs: 2 (5 - 3)
+
+
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  },
+  greet: function() {
+    return "Hello, my name is " + this.fullName();
+  }
+};
+
+
+let account = {
+  balance: 0,
+  deposit: function(amount) {
+    this.balance += amount;
+    return "Deposit of $" + amount + " successful. Current balance: $" + this.balance;
+  },
+  withdraw: function(amount) {
+    if (amount <= this.balance) {
+      this.balance -= amount;
+      return "Withdrawal of $" + amount + " successful. Current balance: $" + this.balance;
+    } else {
+      return "Insufficient funds.";
+    }
+  }
+};
+
+
+let product = {
+  name: "Smartphone",
+  price: 499.99,
+  discountedPrice: function(discountPercent) {
+    let discountAmount = this.price * (discountPercent / 100);
+    return this.price - discountAmount;
+  }
+};
